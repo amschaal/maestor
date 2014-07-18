5c5cd36dc2ea830efc55de955a4c33042dc06340
adam <amschaal@ucdavis.edu>
2014-07-18 16:10:57 -0700

    Cleaning up some defunct code.  Leaving some behind temporarily in case it is useful.

--- maestor/flags.py
+++ maestor/flags.py
@@ -58,3 +58,34 @@ def generate_disk_flag(disk,flag,value,time):
             obj.save()
         except:
             obj = DiskFlag.objects.create(disk=disk,flag=flag,flag_range=range_match,value=value,time=time)
+
+#NOT USED: Generate DiskFlags based on the all time min/max from IOStat (need to add more logic for smartctl)
+#Leaving here for code reference
+def generate_flags(flags):
+    from django.db.models import Max, Min
+    for flag in flags:
+        aggregate_func = Max if flag.bad_value == 'high' else Min 
+        DiskFlag.objects.filter(flag=flag).delete()
+        if flag.iostat_attr:
+            
+            #For each level from minor to severe, see if any disks match the criteria
+            for level in FlagRange.WARNING_LEVEL_CHOICES:
+                query= {'name':flag.iostat_attr}
+                try:
+                    range = flag.ranges.get(level=level[0])
+                    if range.minimum:
+                        query['value__gte'] = range.minimum
+                    if range.maximum:
+                        query['value__lte'] = range.maximum
+                    for result in IOStat.objects.filter(**query).values('disk').annotate(value=(aggregate_func('value'))):
+                        try:
+                            obj = DiskFlag.objects.get(disk_id=result['disk'],flag=flag)
+                            obj.value = result.value
+                            obj.save()
+                        except:
+                            obj = DiskFlag.objects.create(disk_id=result['disk'],flag=flag,flag_range=range,value=result['value'])
+                        
+                except Exception, e:
+                    print  level[0]
+                    print e
+                        
